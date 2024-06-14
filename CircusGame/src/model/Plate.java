e5907729cd20f19c77fd82a49fc67a83a63e6532
Mohamed Morsy <93948252+Morsy234@users.noreply.github.com>
2024-06-14 10:37:17 +0300

    Add files via upload

---
+++ CircusGame/src/model/Plate.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author pc
+ */
+public class Plate extends AbstractGameObject {
+
+    //0---->moving
+    //1---->controlled lefthand
+    
+
+    public Plate(State state, String imageName) {
+        super(state ,imageName);
+        
+    }
+
+    
+
+    
+
+}
